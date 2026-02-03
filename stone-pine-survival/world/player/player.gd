76dd6a3dd73e3a1410bec3ee4ad5965edadb5018
Kaytee Tiede <69981789+orcakt@users.noreply.github.com>
2026-02-03 13:09:49 -0700

    Addition of character controls, and day/night cycle. (#3)

---
+++ stone-pine-survival/world/player/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+
+
+func _physics_process(delta):
+	player_movement(delta)
+
+
+func player_movement(delta):
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	velocity = direction * SPEED
+	if Input.is_action_pressed("ui_left"):
+		play_anim("left")
+	elif Input.is_action_pressed("ui_right"):
+		play_anim("right")
+	elif Input.is_action_pressed("ui_down"):
+		play_anim("down")
+	elif Input.is_action_pressed("ui_up"):
+		play_anim("up")
+	else:
+		play_anim("idle")
+	
+	move_and_slide()
+
+func play_anim(direction):
+	var anim = $AnimatedSprite2D
+	
+	match direction:
+		"right":
+			anim.flip_h = false
+			anim.play("walk_side")
+		"left":
+			anim.flip_h = true
+			anim.play("walk_side")
+		"down":
+			anim.play("walk_down")
+		"up":
+			anim.play("walk_up")
+		"idle":
+			anim.play("idle")
