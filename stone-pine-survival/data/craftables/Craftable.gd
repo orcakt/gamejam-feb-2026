e218cda86aaf501f6ca02108d82bf8780ab10b7f
Wake1st <joel.wakefield.dev@gmail.com>
2026-02-04 00:21:30 -0700

    setup item and craftable types

---
+++ stone-pine-survival/data/craftables/Craftable.gd
@@ -0,0 +1,10 @@
+class_name Craftable
+extends Resource
+
+
+@export var name: String
+@export var texture: Texture2D
+@export var sound: AudioStreamWAV
+@export var wieldable: bool
+
+@export var inputs: Dictionary[Item, int]
