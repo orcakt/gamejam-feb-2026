4481cb4e005ac416f255153da9ba4dc3d3f876de
Wake1st <joel.wakefield.dev@gmail.com>
2026-02-04 21:48:22 -0700

    fixed inventory bugs

--- stone-pine-survival/ui/game/inventory_ui.gd
+++ stone-pine-survival/ui/game/inventory_ui.gd
@@ -30,9 +30,15 @@ func _handle_item_updated(item: Item, value: int) -> void:
 			ITEM_SLOT_TEXT % [item.name, value], 
 			item.texture
 		)
-	elif value == 0:
-		item_list.remove_item(item_dict[item])
+	elif value <= 0:
+		var removed_index = item_dict[item]
+		item_list.remove_item(removed_index)
 		item_dict.erase(item)
+		
+		# shift higher indicies
+		for key in item_dict.keys():
+			if removed_index < item_dict[key]:
+				item_dict[key] -= 1
 	else:
 		item_list.set_item_text(
 			item_dict[item],
